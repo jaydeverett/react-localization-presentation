1ef37b0e0066b29777b05056a3c974af85f6024b
Jay Deverett <jayjdeverett@gmail.com>
2018-03-18 22:15:37 -0400

    add emojis to localization

--- client/components/localization/got.js
+++ client/components/localization/got.js
@@ -2,29 +2,23 @@ module.exports = {
 
     "photoOne": {
       name: "got",
-      caption: "Bros heading to battle in Westeros",
+      caption: "Bros heading to battle in Westeros 🏹🏞🏴‍☠️",
       display_src: "got1"
     },
     "photoTwo": {
       name: "got",
-      caption: "Hodor carrying Bran",
+      caption: "💪🏼 Hodor carrying Bran 💪🏼",
       display_src: "got2",
     },
     "photoThree": {
       name: "got",
-      caption: "Caring siblings",
+      caption: "Caring siblings ‍👩‍❤️‍👨",
       display_src: "got3"
     },
     "photoFour": {
       name: "got",
-      caption: "A Queen and her Dragon",
+      caption: "A Queen and her Dragon 🐉",
       display_src: "got4"
     }
 
 }
-
-
-// "Come vuoi il tuo uovo oggi?",
-// "boiledEgg":"Uovo sodo",
-// "softBoiledEgg":"Uovo alla coque",
-// "choice":"Come scegliere l'uovo"
--- client/components/localization/shrek.js
+++ client/components/localization/shrek.js
@@ -1,22 +1,22 @@
 module.exports = {
     photoOne: {
       name: "shrek",
-      caption: "shrek",
+      caption: "Bros heading into battle in Far, Far Away 🏹💫",
       display_src: "shrek1"
     },
     photoTwo: {
       name: "shrek",
-      caption: "shrek",
+      caption: "💪🏼 Gingerbread Man carrying donkey 💪🏼🐐",
       display_src: "shrek2"
     },
     photoThree: {
       name: "shrek",
-      caption: "shrek",
+      caption: "So much love ‍👩‍❤️‍👨",
       display_src: "shrek3"
     },
     photoFour: {
       name: "shrek",
-      caption: "shrek",
+      caption: "A Donkey and his Dragon 🐉",
       display_src: "shrek4"
     }
 }
